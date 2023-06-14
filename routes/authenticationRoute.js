c7e54eb5fb0e77286801ea3684a625eb1adf55ec
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-14 19:31:30 +0900

    Add name and password confirmation fields in signup form

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.get("/sign-up", (req, res, next) => {
   res.render("sign-up");
+});
+
+router.post("/sign-up", (req, res, next) => {
+  try {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      } else {
+        const user = new User({});
+      }
+    });
+  } catch (err) {}
 });
 
 module.exports = router;
