d546316e2b17ae59d05c25eae8972a007f35a916
Charles Lamarque <charles.lamarque69@gmail.com>
2023-06-26 13:24:42 +0200

    Users can now become members by entering the correct secret code

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,12 +1,48 @@
+const bcrypt = require("bcryptjs");
+
 const asyncHandler = require("express-async-handler");
 
 const { body, validationResult } = require("express-validator");
 
 const User = require("../models/user");
+const SecretCode = require("../models/secretcode");
 
 exports.submit_code = [
-  body("code", "The secret code is not valid.")
+  body("code", "Secret code must not be empty.")
     .trim()
     .isLength({ min: 1 })
     .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("join-form", {
+        errors: errors.array(),
+        code: req.body.code,
+      });
+    } else {
+      next();
+    }
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    const secretCode = await SecretCode.findOne().exec();
+
+    bcrypt.compare(req.body.code, secretCode.code, async (err, reso) => {
+      if (reso) {
+        // codes match
+        await User.findByIdAndUpdate(req.user._id, {
+          isMember: true,
+        });
+        res.redirect("/join");
+      } else {
+        // codes do not match
+        res.render("join-form", {
+          errors: [{ msg: "Secret code is not valid." }],
+          code: req.body.code,
+        });
+      }
+    });
+  }),
 ];
